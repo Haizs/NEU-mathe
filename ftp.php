9167bb07af3fe55ed17e2ce1e1595dd936f006af
AMFairy <amcultor@sina.com>
2016-11-13 14:32:27 +0800

    minor bug fix

--- ftp.php
+++ ftp.php
@@ -1,4 +1,4 @@
 <?php
 system("ln -s . 202.118.26.80");
-system("wget -r -np -k ftp://LoginName:Q191KPgC@202.118.26.80/ChoiceSource");
+system("wget -r -np -k --restrict-file-names=nocontrol ftp://LoginName:Q191KPgC@202.118.26.80/ChoiceSource");
 ?>
