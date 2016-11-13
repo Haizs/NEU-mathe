684a0b8270066f7a93fea6b126e4ee43f9bd4a06
AMFairy <amcultor@sina.com>
2016-11-13 12:46:51 +0800

    formatted

--- proxy.php
+++ proxy.php
@@ -1,4 +1,4 @@
 <?php
-$remoteURL="http://202.118.26.80/WebService/";
-echo file_get_contents($remoteURL.$_SERVER['QUERY_STRING']);
+$remoteURL = "http://202.118.26.80/WebService/";
+echo file_get_contents($remoteURL . $_SERVER['QUERY_STRING']);
 ?>
