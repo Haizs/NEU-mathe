1a2f3315fcd7f6ef0081cf9ffaaf9e896159eac9
Haizs <Jmas2711@gmail.com>
2017-03-16 01:05:46 +0800

    Add advancedSetting

--- config.php
+++ config.php
@@ -5,7 +5,5 @@ $host = 'localhost';
 $dbName = 'mathe';
 $user = 'mathe';
 $pass = 'mathe';
-$dsn = "$dbms:host=$host;dbname=$dbName";
 
-$conn = new PDO($dsn, $user, $pass, array(PDO::ATTR_PERSISTENT => true));
-$conn->query('set names utf8;');
+$dsn = "$dbms:host=$host;dbname=$dbName;charset=utf8";
--- getChapter.php
+++ getChapter.php
@@ -1,3 +1,25 @@
 <?php
+if (!isset($_GET['subject'])) {
+    http_response_code(400);
+    die();
+}
 require_once('config.php');
-$sql = 'SELECT * FROM ' . substr(trim($_GET['subject']), 0, 2) . '_Chapter';
+try {
+    $dbh = new PDO($dsn, $user, $pass);
+    $sql = 'SELECT * FROM ' . substr(trim($_GET['subject']), 0, 2) . '_Chapter ORDER BY Id ASC';
+    $sth = $dbh->prepare($sql);
+    $sth->execute();
+} catch (PDOException $e) {
+    http_response_code(500);
+    die();
+}
+$sth->setFetchMode(PDO::FETCH_ASSOC);
+$chapters = array();
+$pointer = array();
+$pointer[0] =& $chapters;
+while ($row = $sth->fetch()) {
+    $pid = intval($row['Pid']);
+    $pointer[$pid][] = array('id' => intval($row['Id']), 'name' => $row['Name'], 'children' => array());
+    $pointer[intval($row['Id'])] =& $pointer[$pid][count($pointer[$pid]) - 1]['children'];
+}
+echo str_replace(',"children":[]', '', json_encode($chapters));
